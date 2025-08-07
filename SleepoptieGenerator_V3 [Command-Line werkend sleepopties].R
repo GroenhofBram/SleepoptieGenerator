4771cc0e099608805a16627ea6ce9b19e4203230
シカ <158333970+GroenhofBram@users.noreply.github.com>
2025-08-07 11:03:48 +0200

    Werkt nu voor Gapmatch Categorize.

--- SleepoptieGenerator_V3 [Command-Line werkend sleepopties].R
+++ SleepoptieGenerator_V3 [Command-Line werkend sleepopties].R
@@ -1,18 +1,22 @@
 ### Libraries ##################################################################
+packages <- c("magick", "stringr", "tidyverse", "readxl", "writexl")
+
+# Install packages if they are not already installed
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg)
+  }
+}
+
+# Load the libraries
 library(magick)
 library(stringr)
 library(tidyverse)
 library(readxl)
 library(writexl)
 
-#### Mogelijke bugs/glitches [VOOR BRAM OM NIET TE VERGETEN] ####
-# 1: Plaatje is niet hoog genoeg: max_number_of_lines moet anders berekend worden
-# 2: Sleepopties worden te hoog, terwijl er veel ruimte overblijft op regels: max_chars_per_line uitproberen
-####
-
-
 ### Functions ##################################################################
-# Function to wrap text to a specified width (remains unchanged)
+# Function to wrap text to a specified width
 wrap_text <- function(text, width) {
   lines_for_curr_text <- 0
   words <- unlist(str_split(text, " "))
@@ -34,13 +38,13 @@ wrap_text <- function(text, width) {
   return(list(wrapped_text = paste(wrapped_lines, collapse = "\n"), line_count = lines_for_curr_text))
 }
 
-# Function to load data from an Excel file (remains unchanged)
+# Function to load data from an Excel file
 load_data <- function(file_path) {
   data <- read_excel(file_path)
   return(data)
 }
 
-# Function to prepare sleep options from selected row (remains unchanged)
+# Function to prepare sleep options from selected row
 prepare_sleep_options <- function(selected_row) {
   sleepopties <- list(
     sleepoptie_A = selected_row$sleepoptie_A,
@@ -55,52 +59,50 @@ prepare_sleep_options <- function(selected_row) {
   return(sleepopties[sapply(sleepopties, function(x) !is.na(x) && nchar(x) > 0)])
 }
 
-# Mapje maken/checken of mapje bestaat
-create_output_dir <- function(directory_name) {
-  if (dir.exists(directory_name)) {
+# Create output directory if it doesn't exist
+create_output_dir <- function(full_output_directory) {
+  if (dir.exists(full_output_directory)) {
     cat("\n####################################################\n",
-        "Er bestaat al een map voor:", directory_name, "\n --> Dit is gek omdat de kolom 'gegenereerd leeg is.\n",
-        "!!!!!", "Zorg ervoor dat de map leeg is, of dat de plaatjes/bestanden die daar al instaan weer in een map daarbinnen staan.\n",
-        "Wanneer je door wil gaan, geef een willekeurige letter of willekeurig woord op.\n")
+        "Er bestaat al een map voor:", full_output_directory, "\n --> Dit is gek omdat de kolom 'gegenereerd' leeg is.\n",
+        "!!!!!", "Maak een sub-map in deze map en zet daar de plaatjes in die er nu al instaan, anders worden deze plaatjes vervangen door de nieuwe\n",
+        "Wanneer je dit gedaan hebt, type dan een willekeurige letter en druk op enter.\n")
     user_input <- readline("Geef hier een willekeurige letter/willekeruig woord op: ")
     cat("\n####################################################\n")
+    dir.create(full_output_directory)
     if (nchar(user_input) == 0) {
       stop("De code stopt.")
     }
   } else {
-    dir.create(directory_name)
+    dir.create(full_output_directory)
   }
 }
 
 # Function to create images for sleep options
-create_images <- function(non_empty_sleepopties, tekst_titel, tekst_itemnummer, max_chars_per_line) {
-  max_number_of_lines <- 0
+create_images <- function(non_empty_sleepopties, full_output_directory, tekst_itemnummer, max_chars_per_line) {
+  max_number_of_lines <- 0  
   # Iterate over each sleep option
   for (var_name in names(non_empty_sleepopties)) {
     text_to_wrap <- non_empty_sleepopties[[var_name]]
     result <- wrap_text(text_to_wrap, max_chars_per_line)
     wrapped_text <- str_trim(result$wrapped_text)
     lines_needed <- result$line_count
-    
     # Update maximum number of lines based on actual needed lines
     if (lines_needed > max_number_of_lines) {
-      max_number_of_lines <- lines_needed  # 
-      #cat("MAX NUMBER OF LINES UPDATED TO: ", max_number_of_lines, "=====\n")
+      max_number_of_lines <- lines_needed 
     }
   }
   
   # Berekening van hoogte voor plaatjes
   calculated_height <- (max_number_of_lines * 15) + 5
-  
   for (var_name in names(non_empty_sleepopties)) {
     text_to_wrap <- non_empty_sleepopties[[var_name]]
     result <- wrap_text(text_to_wrap, max_chars_per_line)
     wrapped_text <- str_trim(result$wrapped_text)
-    
     # Plaatje maken met de berekende hoogte
     sleepoptie_doos_img <- image_read("500x500_template.png") %>%
       image_resize(paste0("210x", calculated_height, "!")) %>%
       image_background("white")
+    
     sleepoptie_doos <- image_annotate(sleepoptie_doos_img,
                                       wrapped_text,
                                       size = 11,
@@ -108,18 +110,18 @@ create_images <- function(non_empty_sleepopties, tekst_titel, tekst_itemnummer, 
                                       color = "black",
                                       font = "Verdana")
     
-    output_filename <- file.path(tekst_titel, paste0(tekst_titel, var_name, ".jpg"))
+    output_filename <- file.path(full_output_directory, paste0(tekst_titel, "_", tekst_itemnummer, "_", var_name, ".jpg")) # Use full_output_directory here
     image_write(sleepoptie_doos, output_filename)
   }
-  cat("Sleepopties gegenereerd voor: ", tekst_titel,"\n")
+  cat("Sleepopties gegenereerd voor: ", tekst_titel,tekst_itemnummer, "\n")
 }
 
 ### Main Script ################################################################
 file_path <- "Sleepvraag_Items.xlsx"
 data <- load_data(file_path)
+
 user_name <- readline(prompt = "Geef a.u.b. jouw naam: ")
 today_date <- format(Sys.Date(), "%Y-%m-%d")
-
 # Alle regels waar 'gegenereerd' leeg is uitvoeren
 for (i in seq_len(nrow(data))) {
   if (is.na(data$gegenereerd[i]) || nchar(data$gegenereerd[i]) == 0) {
@@ -130,15 +132,25 @@ for (i in seq_len(nrow(data))) {
     max_chars_per_line <- 33 ## 33, uitzondering nagelaten
     non_empty_sleepopties <- prepare_sleep_options(selected_row)
     
-    # Folder aanmaken
-    output_directory_name <- paste0(vak, "_", tekst_titel, "_item_", tekst_itemnummer)
-    create_output_dir(output_directory_name)  # Updated directory name
-    create_images(non_empty_sleepopties, output_directory_name, tekst_itemnummer, max_chars_per_line)
+    # Create the main output directory if it doesn't exist
+    main_output_directory <- "Sleepvragen_plaatjes"
+    if (!dir.exists(main_output_directory)) {
+      dir.create(main_output_directory)
+    }
     
-    # gegenereerd kolom updaten, om overschrijfproblemen te voorkomen
+    # Create the output directory name and full output directory path
+    output_directory_name <- paste0(vak, "_", tekst_titel, "_item_", tekst_itemnummer)
+    full_output_directory <- file.path(main_output_directory, output_directory_name)
+    
+    create_output_dir(full_output_directory)  # Check and create output directory
+    create_images(non_empty_sleepopties, full_output_directory, tekst_itemnummer, max_chars_per_line)  # Updated function call
+    
+    # Update 'gegenereerd' column to prevent overwriting issues
     data$gegenereerd[i] <- paste(today_date, "door", user_name)
   }
 }
-# gegeneerd kolom ook updaten naar Excel
+
+# Update 'gegenereerd' column in the Excel file
 write_xlsx(data, file_path)
 cat("Alle sleepopties zijn gegenereerd, dankjewel", user_name, "! :)")
+cat("\n !!!!! Warning messages kun je negeren !!!!!")
